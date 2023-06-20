6d520d8ac32bf3700ed73dabccaa58b5a97f43e6
Anish <anishnatekar@gmail.com>
2023-06-20 16:32:04 +0530

    Update Motor_testing.ino

--- Software/Testing/Motor_testing/Motor_testing.ino
+++ Software/Testing/Motor_testing/Motor_testing.ino
@@ -1,4 +1,3 @@
-
 #define MOT_TOP_LEFT 18
 #define MOT_TOP_RIGHT 13
 #define MOT_BOTTOM_LEFT 28
@@ -17,28 +16,25 @@ void setup() {
   analogWrite(MOT_TOP_RIGHT, 0);
   analogWrite(MOT_BOTTOM_LEFT, 0);
   analogWrite(MOT_BOTTOM_RIGHT, 0);
-  
-  //Use the led on the Arduino for startup indication.
-  digitalWrite(LED_BUILTIN,HIGH);              
-}
 
+  //Use the led on the Arduino for startup indication.
+  digitalWrite(LED_BUILTIN,HIGH);
+}
+// take props off before uploading script
 void loop() {
   // put your main code here, to run repeatedly:
-  for(int i=1000; i<=2000; i++) {
-    
-  map(i, 1000, 2000, 0, 1000);
-      analogWrite(MOT_TOP_RIGHT, i);
-  analogWrite(MOT_BOTTOM_RIGHT, i);
-  analogWrite(MOT_BOTTOM_LEFT, i);
-  analogWrite(MOT_TOP_LEFT, i);
-  delay(2);
+  for(int i=0; i<=1000; i++) {
+    analogWrite(MOT_TOP_RIGHT, i);
+    analogWrite(MOT_BOTTOM_RIGHT, i);
+    analogWrite(MOT_BOTTOM_LEFT, i);
+    analogWrite(MOT_TOP_LEFT, i);
+    delay(2);
   }
-  for(int i=1000; i<=2000; i++) {
-    
-      analogWrite(MOT_TOP_RIGHT, 1000-i);
-  analogWrite(MOT_BOTTOM_RIGHT, 1000-i);
-  analogWrite(MOT_BOTTOM_LEFT, 1000-i);
-  analogWrite(MOT_TOP_LEFT, 1000-i);
-  delay(2);
+  for(int i=0; i<=1000; i++) {
+    analogWrite(MOT_TOP_RIGHT, 1000-i);
+    analogWrite(MOT_BOTTOM_RIGHT, 1000-i);
+    analogWrite(MOT_BOTTOM_LEFT, 1000-i);
+    analogWrite(MOT_TOP_LEFT, 1000-i);
+    delay(2);
   }
 }
